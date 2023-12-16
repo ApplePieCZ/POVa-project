7aa61e36f14e399eeb5d3da7fbd0406ac2ddc458
ApplePieCZ <lukas1454@seznam.cz>
2023-12-16 20:57:59 +0100

    Update sift.py
    Upgraded SIFT to run in parallel on cpu.

--- sift.py
+++ sift.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import time
+from concurrent.futures import ThreadPoolExecutor
 
-#SIFT COMPUTE
+
 def compute_sift(image_path):
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -12,26 +14,34 @@ def compute_sift(image_path):
     return image_keypoints, image_descriptors
 
 
+def process_image(image):
+    _, descriptors = compute_sift(f"{reference_images_path}/{image}")
+    matches = bf.match(query_descriptors, descriptors)
+    return len(matches)
+
+
 if __name__ == "__main__":
     query_image_path = 'jpg/image_00001.jpg'
     reference_images_path = 'flowers/flowers-102/jpg'
+    start_time = time.time()
 
     query_image = cv2.imread(query_image_path)
-    query_keypoints, query_descriptors = compute_sift(query_image_path)
+    _, query_descriptors = compute_sift(query_image_path)
 
-    jpg_files = [f for f in os.listdir(reference_images_path) if f.endswith('.jpg')][:500]
+    jpg_files = [f for f in os.listdir(reference_images_path) if f.endswith('.jpg')][:1000]
 
     bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
 
-    images_matches = []
+    #images_matches = []
 
-    for image in jpg_files:
-        keypoints, descriptors = compute_sift(f"{reference_images_path}/{image}")
-        matches = bf.match(query_descriptors, descriptors)
-        images_matches.append(len(matches))
+    with ThreadPoolExecutor() as executor:
+        # Process images in parallel and collect descriptors
+        images_matches = list(executor.map(process_image, jpg_files))
+
+    #np.save("descriptors.npy", np.array(descriptors_list, dtype=object))
+    print(time.time() - start_time)
 
     closest_images = np.argsort(images_matches)[-6:]
-    print(closest_images)
 
     fig, axes = plt.subplots(2, 3, figsize=(8, 8))
 
