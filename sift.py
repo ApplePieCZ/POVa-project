9807e3cca3df4079bb14332469306ae4e07344c6
ApplePieCZ <lukas1454@seznam.cz>
2023-12-16 22:37:56 +0100

    Update sift.py
    Added arguments integration.

--- sift.py
+++ sift.py
@@ -1,9 +1,12 @@
+# POVa Project - CBIR
+# Lukas Marek, with help of Tomas Krsicka
+# 17.12.2023
 import cv2
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-import time
 from concurrent.futures import ThreadPoolExecutor
+import argparse
 
 
 def compute(image_path):
@@ -20,43 +23,47 @@ def process_image(image):
 
 
 if __name__ == "__main__":
-    sift = True
+    parser = argparse.ArgumentParser(description="SIFT/ORB based CBIR.")
+    parser.add_argument("-i", type=str, help="Query image")
+    parser.add_argument("-d", type=str, help="Path to reference images")
+    parser.add_argument("-c", type=int, help="Path to reference images")
+    parser.add_argument("-s", action='store_true', help="Path to reference images")
 
-    if sift:
+    args = parser.parse_args()
+
+    if args.s:
         algo = cv2.SIFT_create()
     else:
         algo = cv2.ORB_create()
 
-    query_image_path = 'jpg/image_00001.jpg'
-    reference_images_path = 'flowers/flowers-102/jpg'
-    start_time = time.time()
+    if args.c:
+        clip = args.c
+    else:
+        clip = 1000
+
+    query_image_path = args.i
+    reference_images_path = args.d
 
     query_image = cv2.imread(query_image_path)
     query_descriptors = compute(query_image_path)
 
-    jpg_files = [f for f in os.listdir(reference_images_path) if f.endswith('.jpg')][:1000]
+    jpg_files = [f for f in os.listdir(reference_images_path) if f.endswith('.jpg')][:clip]
 
     bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
 
-    #images_matches = []
-
     with ThreadPoolExecutor() as executor:
-        # Process images in parallel and collect descriptors
         images_matches = list(executor.map(process_image, jpg_files))
-
-    #np.save("descriptors.npy", np.array(descriptors_list, dtype=object))
-    print(time.time() - start_time)
 
     closest_images = np.argsort(images_matches)[-6:]
 
     fig, axes = plt.subplots(2, 3, figsize=(8, 8))
 
-    axes[0, 0].imshow(plt.imread(f"flowers/flowers-102/jpg/{jpg_files[closest_images[5]]}"))
-    axes[0, 1].imshow(plt.imread(f"flowers/flowers-102/jpg/{jpg_files[closest_images[4]]}"))
-    axes[0, 2].imshow(plt.imread(f"flowers/flowers-102/jpg/{jpg_files[closest_images[3]]}"))
-    axes[1, 0].imshow(plt.imread(f"flowers/flowers-102/jpg/{jpg_files[closest_images[2]]}"))
-    axes[1, 1].imshow(plt.imread(f"flowers/flowers-102/jpg/{jpg_files[closest_images[1]]}"))
-    axes[1, 2].imshow(plt.imread(f"flowers/flowers-102/jpg/{jpg_files[closest_images[0]]}"))
+    axes[0, 0].imshow(plt.imread(f"{reference_images_path}/{jpg_files[closest_images[5]]}"))
+    axes[0, 1].imshow(plt.imread(f"{reference_images_path}/{jpg_files[closest_images[4]]}"))
+    axes[0, 2].imshow(plt.imread(f"{reference_images_path}/{jpg_files[closest_images[3]]}"))
+    axes[1, 0].imshow(plt.imread(f"{reference_images_path}/{jpg_files[closest_images[2]]}"))
+    axes[1, 1].imshow(plt.imread(f"{reference_images_path}/{jpg_files[closest_images[1]]}"))
+    axes[1, 2].imshow(plt.imread(f"{reference_images_path}/{jpg_files[closest_images[0]]}"))
 
     for ax in axes.flatten():
         ax.axis('off')
