15a8bc67229a5de226b091885c98d40294c85d2c
ApplePieCZ <lukas1454@seznam.cz>
2023-12-20 14:04:07 +0100

    Update sift.py
    Implemented knn and flann based matchers for SIFT and ORB.

--- sift.py
+++ sift.py
@@ -1,6 +1,6 @@
 # POVa Project - CBIR
 # Lukas Marek, with help of Tomas Krsicka
-# 17.12.2023
+# 20.12.2023
 import cv2
 import numpy as np
 import os
@@ -17,10 +17,42 @@ def compute(image_path):
     return image_descriptors
 
 
-def process_image(image):
+def process_image_norm(image):
     descriptors = compute(f"{reference_images_path}/{image}")
-    matches = bf.match(query_descriptors, descriptors)
+    matches = bf_norm.match(query_descriptors, descriptors)
     return len(matches)
+
+
+def process_image_knn(image):
+    descriptors = compute(f"{reference_images_path}/{image}")
+    matches = bf.knnMatch(query_descriptors, descriptors, k=2)
+    a = 0
+    for m, n in matches:
+        if m.distance < 0.7 * n.distance:
+            a += 1
+    return a
+
+
+def process_image_flann_orb(image):
+    descriptors = compute(f"{reference_images_path}/{image}")
+    matches = flann_orb.knnMatch(query_descriptors, descriptors, k=2)
+    a = 0
+    for i, match in enumerate(matches):
+        if len(match) >= 2:
+            m, n = match
+            if m.distance < 0.95 * n.distance:
+                a += 1
+    return a
+
+
+def process_image_flann(image):
+    descriptors = compute(f"{reference_images_path}/{image}")
+    matches = flann.knnMatch(query_descriptors, descriptors, k=2)
+    a = 0
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.95 * n.distance:
+            a += 1
+    return a
 
 
 if __name__ == "__main__":
@@ -45,16 +77,30 @@ if __name__ == "__main__":
     query_image_path = args.i
     reference_images_path = args.d
 
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    FLANN_INDEX_LSH = 6
+    index2_params = dict(algorithm=FLANN_INDEX_LSH,
+                         table_number=12,  # 12
+                         key_size=20,  # 20
+                         multi_probe_level=2)  # 2
+    flann_orb = cv2.FlannBasedMatcher(index2_params, search_params)
+
     start_time = time.time()
     query_image = cv2.imread(query_image_path)
     query_descriptors = compute(query_image_path)
 
     jpg_files = [f for f in os.listdir(reference_images_path) if f.endswith('.jpg')][:clip]
 
-    bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+    bf_norm = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+
+    bf = cv2.BFMatcher()
 
     with ThreadPoolExecutor() as executor:
-        images_matches = list(executor.map(process_image, jpg_files))
+        images_matches = list(executor.map(process_image_flann_orb, jpg_files))
 
     print(f"{time.time() - start_time:.2f}")
 
@@ -73,6 +119,3 @@ if __name__ == "__main__":
         ax.axis('off')
 
     plt.show()
-
-
-
